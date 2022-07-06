423c4ec4a5ff667aafe2356f2f3253a67fb602ff
Miguel Ibáñez <miguelibaezgarcia@gmail.com>
2022-07-06 18:31:17 +0200

    Add files via upload

--- fisher_info.py
+++ fisher_info.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import statistics as st
 
 pi=3.14159265359 
+kB = 1.380649e-23
 
 ##################################
 ##################################
@@ -26,11 +27,11 @@ def trayectoria(filename,fs,ncycles,num_files,dttemp,ntrans,margen):
     pts_med_eq[1] = npoints_per_cycle - 21 #Eq
     
     pts_med_transO = np.zeros(2*ncycles)
-    pts_med_transO[0] = int(dttemp*fs+3)
+    pts_med_transO[0] = int(dttemp*fs)
     pts_med_transO[1] = pts_med_transO[0] + ntrans
     
     pts_med_transR = np.zeros(2*ncycles)
-    pts_med_transR[0] = int(2*dttemp*fs+6)
+    pts_med_transR[0] = int(2*dttemp*fs)
     pts_med_transR[1] = pts_med_transR[0] + ntrans
     
     npts_eq = int(npoints_per_semicycle-npoints_margin-21)
@@ -38,6 +39,9 @@ def trayectoria(filename,fs,ncycles,num_files,dttemp,ntrans,margen):
     
     trans_array = np.zeros((ntrans,ncycles*num_files))
     trans_arrayR = np.zeros((ntrans,ncycles*num_files))
+    
+    noise = np.zeros((ntrans,ncycles*num_files))
+    noiseR = np.zeros((ntrans,ncycles*num_files))
     
     # Calculadora de puntos a medir
     m = 2
@@ -55,13 +59,17 @@ def trayectoria(filename,fs,ncycles,num_files,dttemp,ntrans,margen):
         
         m = 0
         x = np.zeros(npoints_per_fileR)
+        y = np.zeros(npoints_per_fileR) # Noisy signal
+        
         for i in range(8):
             fid, next(fid)
         while m <= npoints_per_fileR-1:
             line = fid.readline()
             line_split = line.split()
             ls1 = line_split[1]
+            ls13 = line_split[12]
             x[m] = float(ls1)
+            y[m] = float(ls13)
             
             m = m + 1
         
@@ -76,10 +84,13 @@ def trayectoria(filename,fs,ncycles,num_files,dttemp,ntrans,margen):
             tfi = int(pts_med_eq[m+1])
             tfi1 = int(pts_med_transO[m+1])
             tfi3 = int(pts_med_transR[m+1])
-       
+            
             xsec_eq = x[tin:tfi]
             xsec_tr = x[tin1:tfi1]
             xsec_trR = x[tin3:tfi3]
+            
+            ysec_tr = y[tin1:tfi1]
+            ysec_trR = y[tin3:tfi3]
             
             i0=int(mm*npts_eq)
             i1=int((mm+1)*npts_eq)
@@ -87,13 +98,16 @@ def trayectoria(filename,fs,ncycles,num_files,dttemp,ntrans,margen):
             trans_array[:,mm] = xsec_tr
             trans_arrayR[:,mm] = xsec_trR
             
+            noise[:,mm] = ysec_tr
+            noiseR[:,mm] = ysec_trR
+            
             m=m+2
             mm=mm+1
         
         print('Archivo ' + str(cFile))
         cFile += 1
-        
-    return trans_array,trans_arrayR,eq_series
+   
+    return trans_array,trans_arrayR,eq_series,noise,noiseR
     
 def histogramas(bins,serie):
     hist,bins_hist = np.histogram(serie,bins=bins,density=True)
@@ -222,9 +236,9 @@ def integrales(mDistr,nbins,ntrans,Dx,fs):
 def protocolo(dir_name,opt,fs,ncycles,num_files,dttemp,ntrans,nbins,kappa,Nc,Neq,margen):
     dir_name = dir_name + opt + '/'
     
-    trans_array,trans_arrayR,eq_series = \
+    trans_array,trans_arrayR,eq_series,noise,noiseR = \
         trayectoria(dir_name,fs,ncycles,num_files,dttemp,ntrans,margen)
-        
+    
     bins= np.linspace(eq_series.min()-0.5e-8,eq_series.max()+0.5e-8,num=nbins)
     Dx = bins[1]-bins[0]
 
@@ -237,8 +251,14 @@ def protocolo(dir_name,opt,fs,ncycles,num_files,dttemp,ntrans,nbins,kappa,Nc,Neq
     mDistrR = np.zeros((ntrans,nbins-1))
     #eMatrizR = np.zeros((ntrans,nbins))
     
+    media = np.zeros(ntrans)
+    mediaR = np.zeros(ntrans)
+    varianza = np.zeros(ntrans)
+    varianzaR = np.zeros(ntrans)
+    
     while i < ntrans:
         trans_series = trans_array[i,:]
+        
         hist_trans,bins_trans = histogramas(bins,trans_series)
         #eHist_trans = np.sqrt(hist_trans / (Nc*Dx))
         
@@ -248,7 +268,11 @@ def protocolo(dir_name,opt,fs,ncycles,num_files,dttemp,ntrans,nbins,kappa,Nc,Neq
         mDistr[i,:] = hist_trans
         #eMatriz[i,:] = eHist_trans
         
+        media[i] = np.mean(trans_series)
+        varianza[i] = np.var(trans_series)
+        
         trans_series = trans_arrayR[i,:]
+        
         hist_trans,bins_trans = histogramas(bins,trans_series)
         #eHist_trans = np.sqrt(hist_trans / (Nc*Dx))
         
@@ -258,12 +282,15 @@ def protocolo(dir_name,opt,fs,ncycles,num_files,dttemp,ntrans,nbins,kappa,Nc,Neq
         mDistrR[i,:] = hist_trans
         #eMatrizR[i,:] = eHist_trans
         
+        mediaR[i] = np.mean(trans_series)
+        varianzaR[i] = np.var(trans_series)
+        
         i += 1
 
     infoF,infoF2,infoF3,infoF4 = integrales(mDistr,nbins,ntrans,Dx,fs)
     infoR,infoR2,infoR3,infoR4 = integrales(mDistrR,nbins,ntrans,Dx,fs)
             
-    return infoF,infoF2,infoF3,infoF4,infoR,infoR2,infoR3,infoR4
+    return infoF,infoF2,infoF3,infoF4,infoR,infoR2,infoR3,infoR4,noise,noiseR,media,mediaR,varianza,varianzaR
 
 def dibujos(serieHeat,serieCool,eHeat,eCool,ts,magnitude,opt,fname,N):
     plt.figure()
@@ -301,7 +328,7 @@ margen = 0.003
 ntrans = 100
 #kappa = 41.64e-6 #tanda7
 #kappa = 14.04e-6
-kappa = 79.40e-6
+kappa = 41.64e-6
 
 
 Nc = ncycles*num_files
@@ -320,13 +347,13 @@ tss = np.arange(ntrans)/fs
 
 nbins = 70
 
-infoF,infoF2,infoF3,infoF4,infoR,infoR2,infoR3,infoR4 = \
+infoF,infoF2,infoF3,infoF4,infoR,infoR2,infoR3,infoR4,noise,noiseR,media,mediaR,varianza,varianzaR = \
     protocolo(dir_name,'directo',fs,ncycles,num_files,dttemp,ntrans,nbins,kappa,Nc,Neq,margen)
 
 print('INVERSO')
 
 nbins = 65   
-infoFI,infoFI2,infoFI3,infoFI4,infoRI,infoRI2,infoRI3,infoRI4 = \
+infoFI,infoFI2,infoFI3,infoFI4,infoRI,infoRI2,infoRI3,infoRI4,noise,noiseR,mediaI,mediaRI,varianzaI,varianzaRI = \
     protocolo(dir_name,'inverso',fs,ncycles,num_files,dttemp,ntrans,nbins,kappa,Nc,Neq,margen)    
 
 plt.figure()
@@ -357,28 +384,28 @@ plt.plot(ts,infoRI3,'.')
 plt.plot(ts,infoRI4,'.')
 plt.legend(['Metodo 1','Metodo 2','Metodo 3','Metodo 4'])
 
-fich = open('info.out','w')
+fich = open('info_nuevo.out','w')
 
 for i in range(ntrans-2):
     fich.write(str(ts[i]) + ' ' + str(infoF[i]) + ' ' + str(infoF2[i]) + ' ' + str(infoF3[i]) + ' ' + str(infoF4[i]) + '\n')
     
 fich.close()
 
-fich = open('infoR.out','w')
+fich = open('infoR_nuevo.out','w')
 
 for i in range(ntrans-2):
     fich.write(str(ts[i]) + ' ' + str(infoR[i]) + ' ' + str(infoR2[i]) + ' ' + str(infoR3[i]) + ' ' + str(infoR4[i]) + '\n')
     
 fich.close()
 
-fich = open('infoI.out','w')
+fich = open('infoI_nuevo.out','w')
 
 for i in range(ntrans-2):
     fich.write(str(ts[i]) + ' ' + str(infoFI[i]) + ' ' + str(infoFI2[i]) + ' ' + str(infoFI3[i]) + ' ' + str(infoFI4[i]) + '\n')
     
 fich.close()
 
-fich = open('infoRI.out','w')
+fich = open('infoRI_nuevo.out','w')
 
 for i in range(ntrans-2):
     fich.write(str(ts[i]) + ' ' + str(infoRI[i]) + ' ' + str(infoRI2[i]) + ' ' + str(infoRI3[i]) + ' ' + str(infoRI4[i]) + '\n')
@@ -386,21 +413,60 @@ for i in range(ntrans-2):
 fich.close()
     
  
+# mediaN = np.mean(noise,axis=1)
+# varN = np.var(noise,axis=1)
 
+# plt.figure()
+# plt.plot(np.arange(ntrans)/fs,mediaN)
+
+# plt.figure()
+# plt.plot(np.arange(ntrans)/fs,varN)
+
+# plt.figure()
+# plt.plot(np.arange(ntrans)/fs,media)
+
+# plt.figure()
+# plt.plot(np.arange(ntrans)/fs,kappa*varianza/kB)
    
-# LF = np.zeros(ntrans-2)
-# LR = np.zeros(ntrans-2)
-# LFI = np.zeros(ntrans-2)
-# LRI = np.zeros(ntrans-2)
+LF = np.zeros(ntrans-2)
+LR = np.zeros(ntrans-2)
+LFI = np.zeros(ntrans-2)
+LRI = np.zeros(ntrans-2)
 
-# n=0
-# while n<ntrans-2:
-#     LF[n] = np.sum(np.sqrt(infoF[0:n])/fs)
-#     LR[n] = np.sum(np.sqrt(infoR[0:n])/fs)
-#     LFI[n] = np.sum(np.sqrt(infoFI[0:n])/fs)
-#     LRI[n] = np.sum(np.sqrt(infoRI[0:n])/fs)
+CF = np.zeros(ntrans-2)
+CR = np.zeros(ntrans-2)
+CFI = np.zeros(ntrans-2)
+CRI = np.zeros(ntrans-2)
+
+n=0
+while n<ntrans-2:
+    LF[n] = np.sum(np.sqrt(infoF2[0:n])/fs)
+    LR[n] = np.sum(np.sqrt(infoR2[0:n])/fs)
+    LFI[n] = np.sum(np.sqrt(infoFI2[0:n])/fs)
+    LRI[n] = np.sum(np.sqrt(infoRI2[0:n])/fs)
     
-#     n +=1
+    CF[n] = 0.5*np.sum(infoF2[0:n]/fs)
+    CR[n] = 0.5*np.sum(infoR2[0:n]/fs)
+    CFI[n] = 0.5*np.sum(infoFI2[0:n]/fs)
+    CRI[n] = 0.5*np.sum(infoRI2[0:n]/fs)
+    
+    n +=1
+
+plt.figure()
+plt.plot(ts,ts,'--')
+plt.plot(ts,0.5*np.power(LF,2) / CF,'.')
+
+plt.figure()
+plt.plot(ts,ts,'--')
+plt.plot(ts,0.5*np.power(LR,2) / CR,'.')
+
+plt.figure()
+plt.plot(ts,ts,'--')
+plt.plot(ts,0.5*np.power(LFI,2) / CFI,'.')
+
+plt.figure()
+plt.plot(ts,ts,'--')
+plt.plot(ts,0.5*np.power(LRI,2) / CRI,'.')
 
 # plt.figure()
 # plt.plot(ts,LF,'.',ts,LR,'.')
